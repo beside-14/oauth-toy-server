1379774b1879231b4adb5d36e44dc2cea8743a4a
SeongHeon Sim <deep@osean.me>
2023-03-02 23:36:52 +0900

    #1 feat: JwtAuthenticationFilter 구현
    - UserDetailsService Bean 등록
    - JwtAuthenticationFilter 내에서 유저 정보는 있지만 인증되지 않은 유저에 대한 처리 로직 구현

--- src/main/kotlin/com/bside/v8/global/filter/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/bside/v8/global/filter/JwtAuthenticationFilter.kt
@@ -5,12 +5,17 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.lang.NonNull
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
 class JwtAuthenticationFilter(
-    private val jwtService: JwtService
+    private val jwtService: JwtService,
+    private val userDetailsService: UserDetailsService
 ) : OncePerRequestFilter() {
 
     override fun doFilterInternal(
@@ -18,18 +23,42 @@ class JwtAuthenticationFilter(
         @NonNull response: HttpServletResponse,
         @NonNull filterChain: FilterChain
     ) {
+        // HTTP Header 에서 특정 값을 추출
         val authHeader = request.getHeader("Authorization")
 
-        when (authHeader.isNullOrBlank() || !authHeader.startsWith("Bearer ")) {
-            true -> {
-                filterChain.doFilter(request, response)
-                return
-            }
+        // HTTP Header 에 JWT 토큰을 담은 Key 가 존재하지 않는 다면
+        if (authHeader.isNullOrBlank() || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response)
+            return
+        }
 
-            else -> {
-                val jwt = authHeader.substring(7)
-                val userEmail = jwtService.extractUserName(jwt)
+        // JWT 추출
+        val jwt = authHeader.substring(7)
+
+        // 유저 Email
+        val userEmail = jwtService.extractUserName(jwt)
+
+        // 인증받지 않은 유저인 경우
+        if (userEmail.isNotBlank() && SecurityContextHolder.getContext().authentication == null) {
+            // DB 에서 유저 Email 을 기준으로 유저 데이터 조회 > 존재하지 않으면 예외 발생
+            val userDetails = this.userDetailsService.loadUserByUsername(userEmail)
+
+            // 유저 데이터의 인증 유효성 체크
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                // 인증 토큰 생성
+                val authToken = UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    null, // 아직 인가 받지 않은 유저기 때문에 null 로 설정한다.
+                    userDetails.authorities
+                )
+                // 인증 토큰 디테일 설정 > HTTP Request
+                authToken.details = WebAuthenticationDetailsSource().buildDetails(request)
+
+                // ContextHolder > 인증 정보 세팅
+                SecurityContextHolder.getContext().authentication = authToken
             }
         }
+
+        filterChain.doFilter(request, response)
     }
 }
