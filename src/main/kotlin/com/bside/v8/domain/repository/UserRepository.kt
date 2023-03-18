456084cffeb12041a8c0fa4a8ff5e6f16a43182d
jowonjin <holeman79@gmail.com>
2023-03-18 21:00:19 +0900

    refactor(Platform.kt, UserRepository.kt) : 불필요 클래스 제거

--- src/main/kotlin/com/bside/v8/domain/repository/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.bside.v8.domain.repository
-
-import com.bside.v8.domain.entity.EUser
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.querydsl.QuerydslPredicateExecutor
-import org.springframework.stereotype.Repository
-
-@Repository
-interface UserRepository : JpaRepository<EUser, Long>, QuerydslPredicateExecutor<EUser>
