56e0136eacbf6334b6c7017426eb67a842830fba
jowonjin <holeman79@gmail.com>
2023-03-26 12:40:08 +0900

    refactor(MemberInputException.kt) : code formatting

--- src/main/kotlin/com/bside/v8/domain/member/exception/MemberInputException.kt
+++ src/main/kotlin/com/bside/v8/domain/member/exception/MemberInputException.kt
@@ -2,5 +2,4 @@ package com.bside.v8.domain.member.exception
 
 class MemberInputException(
         override val message: String
-) : RuntimeException("[MemberInputException] $message" ) {
-}
+) : RuntimeException("[MemberInputException] $message")
